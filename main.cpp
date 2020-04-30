53cf90850abe001e7f4551827896e26c10c13967
selfbd <bds_github@fastmail.fm>
2020-04-30 16:26:19 +0000

    Proposed solution complete

--- main.cpp
+++ main.cpp
@@ -39,15 +39,9 @@ send me a DM to check your pull request
  Wait for my code review.
  */
 
-
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-
-// Forward declarations
-struct FloatType;
-struct DoubleType;
-struct IntType;
 
 // FloatType - BEGIN ==================================
 
@@ -58,6 +52,8 @@ struct FloatType
     FloatType(float floatValue) : ownedFloat(new float(floatValue)) {}
     ~FloatType() {delete ownedFloat; ownedFloat = nullptr;}
 
+    operator float() { return *ownedFloat;}
+
     FloatType& add(float rhs);
     FloatType& subtract(float rhs);
     FloatType& multiply(float rhs);
@@ -65,26 +61,7 @@ struct FloatType
 
 private:
     float* ownedFloat;
-
-/*
-    FloatType& add(const FloatType& rhs);
-    FloatType& subtract(const FloatType& rhs);
-    FloatType& multiply(const FloatType& rhs);
-    FloatType& divide(const FloatType& rhs);
-
-    FloatType& add(const DoubleType& rhs);
-    FloatType& subtract(const DoubleType& rhs);
-    FloatType& multiply(const DoubleType& rhs);
-    FloatType& divide(const DoubleType& rhs);
-
-    FloatType& add(const IntType& rhs);
-    FloatType& subtract(const IntType& rhs);
-    FloatType& multiply(const IntType& rhs);
-    FloatType& divide(const IntType& rhs);
-    */
 };
-
-// FloatType member functions with primitive parameters - BEGIN =============
 
 FloatType& FloatType::add(float rhs)
 {
@@ -114,8 +91,6 @@ FloatType& FloatType::divide(float rhs)
     return *this;
 }
 
-// FloatType member functions with primitive parameters - END ===============
-
 // FloatType - END ====================================
 
 // DoubleType - BEGIN =================================
@@ -127,6 +102,8 @@ struct DoubleType
     DoubleType(double doubleValue) : ownedDouble(new double(doubleValue)) {}
     ~DoubleType() {delete ownedDouble; ownedDouble = nullptr;}
 
+    operator double() { return *ownedDouble;}
+
     DoubleType& add(double rhs);
     DoubleType& subtract(double rhs);
     DoubleType& multiply(double rhs);
@@ -134,23 +111,6 @@ struct DoubleType
 
 private:
     double* ownedDouble;
-
-/*
-    DoubleType& add(const FloatType& rhs);
-    DoubleType& subtract(const FloatType& rhs);
-    DoubleType& multiply(const FloatType& rhs);
-    DoubleType& divide(const FloatType& rhs);
-
-    DoubleType& add(const DoubleType& rhs);
-    DoubleType& subtract(const DoubleType& rhs);
-    DoubleType& multiply(const DoubleType& rhs);
-    DoubleType& divide(const DoubleType& rhs);
-
-    DoubleType& add(const IntType& rhs);
-    DoubleType& subtract(const IntType& rhs);
-    DoubleType& multiply(const IntType& rhs);
-    DoubleType& divide(const IntType& rhs);
-    */
 };
 
 DoubleType& DoubleType::add(double rhs)
@@ -192,30 +152,15 @@ struct IntType
     IntType(int intValue) : ownedInt(new int(intValue)) {}
     ~IntType() {delete ownedInt; ownedInt = nullptr;}
 
+    operator int() { return *ownedInt;}
+
     IntType& add(int rhs);
     IntType& subtract(int rhs);
     IntType& multiply(int rhs);
     IntType& divide(int rhs);
-    
+
 private:
     int* ownedInt;
-
-/*
-    IntType& add(const FloatType& rhs);
-    IntType& subtract(const FloatType& rhs);
-    IntType& multiply(const FloatType& rhs);
-    IntType& divide(const FloatType& rhs);
-
-    IntType& add(const DoubleType& rhs);
-    IntType& subtract(const DoubleType& rhs);
-    IntType& multiply(const DoubleType& rhs);
-    IntType& divide(const DoubleType& rhs);
-
-    IntType& add(const IntType& rhs);
-    IntType& subtract(const IntType& rhs);
-    IntType& multiply(const IntType& rhs);
-    IntType& divide(const IntType& rhs);
-    */
 };
 
 IntType& IntType::add(int rhs)
@@ -249,299 +194,59 @@ IntType& IntType::divide(int rhs)
 
 // IntType - END  =====================================
 
-/*
-// FloatType add member function with other UDTs as parameters - BEGIN =======
-
-FloatType& FloatType::add(const FloatType& ft)
-{
-    return add(*ft.ownedFloat);
-}
-
-FloatType& FloatType::add(const DoubleType& dt)
-{
-    return add(*dt.ownedDouble);
-}
-
-FloatType& FloatType::add(const IntType& it)
-{
-    return add(*it.ownedInt);
-}
-
-// FloatType add member function with other UDTs as parameters - END =========
-
-// FloatType subtract member function with other UDTs as parameters - BEGIN ==
-
-FloatType& FloatType::subtract(const FloatType& ft)
-{
-    return subtract(*ft.ownedFloat);
-}
-
-FloatType& FloatType::subtract(const DoubleType& dt)
-{
-    return subtract(*dt.ownedDouble);
-}
-
-FloatType& FloatType::subtract(const IntType& it)
-{
-    return subtract(*it.ownedInt);
-}
-
-// FloatType subtract member function with other UDTs as parameters - END ====
-
-// FloatType multiply member function with other UDTs as parameters - BEGIN ==
-
-FloatType& FloatType::multiply(const FloatType& ft)
-{
-    return multiply(*ft.ownedFloat);
-}
-
-FloatType& FloatType::multiply(const DoubleType& dt)
-{
-    return multiply(*dt.ownedDouble);
-}
-
-FloatType& FloatType::multiply(const IntType& it)
-{
-    return multiply(*it.ownedInt);
-}
-
-// FloatType multiply member function with other UDTs as parameters - END ====
-
-// FloatType divide member function with other UDTs as parameters - BEGIN ====
-
-FloatType& FloatType::divide(const FloatType& ft)
-{
-    return divide(*ft.ownedFloat);
-}
-
-FloatType& FloatType::divide(const DoubleType& dt)
-{
-    return divide(*dt.ownedDouble);
-}
-
-FloatType& FloatType::divide(const IntType& it)
-{
-    return divide(*it.ownedInt);
-}
-
-// FloatType divide member function with other UDTs as parameters - END ======
-
-// DoubleType add member function with other UDTs as parameters - BEGIN ======
-
-DoubleType& DoubleType::add(const FloatType& ft)
-{
-    return add(*ft.ownedFloat);
-}
-
-DoubleType& DoubleType::add(const DoubleType& dt)
-{
-    return add(*dt.ownedDouble);
-}
-
-DoubleType& DoubleType::add(const IntType& it)
-{
-    return add(*it.ownedInt);
-}
-
-// DoubleType add member function with other UDTs as parameters - END ========
-
-// DoubleType subtract member function with other UDTs as parameters - BEGIN =
-
-DoubleType& DoubleType::subtract(const FloatType& ft)
-{
-    return subtract(*ft.ownedFloat);
-}
-
-DoubleType& DoubleType::subtract(const DoubleType& dt)
-{
-    return subtract(*dt.ownedDouble);
-}
-
-DoubleType& DoubleType::subtract(const IntType& it)
-{
-    return subtract(*it.ownedInt);
-}
-
-// DoubleType subtract member function with other UDTs as parameters - END ===
-
-// DoubleType multiply member function with other UDTs as parameters - BEGIN =
-
-DoubleType& DoubleType::multiply(const FloatType& ft)
-{
-    return multiply(*ft.ownedFloat);
-}
-
-DoubleType& DoubleType::multiply(const DoubleType& dt)
-{
-    return multiply(*dt.ownedDouble);
-}
-
-DoubleType& DoubleType::multiply(const IntType& it)
-{
-    return multiply(*it.ownedInt);
-}
-
-// DoubleType multiply member function with other UDTs as parameters - END ===
-
-// DoubleType divide member function with other UDTs as parameters - BEGIN ===
-
-DoubleType& DoubleType::divide(const FloatType& ft)
-{
-    return divide(*ft.ownedFloat);
-}
-
-DoubleType& DoubleType::divide(const DoubleType& dt)
-{
-    return divide(*dt.ownedDouble);
-}
-
-DoubleType& DoubleType::divide(const IntType& it)
-{
-    return divide(*it.ownedInt);
-}
-
-// DoubleType divide member function with other UDTs as parameters - END =====
-
-// IntType add member function with other UDTs as parameters - BEGIN =========
-
-IntType& IntType::add(const FloatType& ft)
-{
-    return add(*ft.ownedFloat);
-}
-
-IntType& IntType::add(const DoubleType& dt)
-{
-    return add(*dt.ownedDouble);
-}
-
-IntType& IntType::add(const IntType& it)
-{
-    return add(*it.ownedInt);
-}
-
-// IntType add member function with other UDTs as parameters - END ===========
-
-// IntType subtract member function with other UDTs as parameters - BEGIN ====
-
-IntType& IntType::subtract(const FloatType& ft)
-{
-    return subtract(*ft.ownedFloat);
-}
-
-IntType& IntType::subtract(const DoubleType& dt)
-{
-    return subtract(*dt.ownedDouble);
-}
-
-IntType& IntType::subtract(const IntType& it)
-{
-    return subtract(*it.ownedInt);
-}
-
-// IntType subtract member function with other UDTs as parameters - END ======
-
-// IntType multiply member function with other UDTs as parameters - BEGIN ====
-
-IntType& IntType::multiply(const FloatType& ft)
-{
-    return multiply(*ft.ownedFloat);
-}
-
-IntType& IntType::multiply(const DoubleType& dt)
-{
-    return multiply(*dt.ownedDouble);
-}
-
-IntType& IntType::multiply(const IntType& it)
-{
-    return multiply(*it.ownedInt);
-}
-
-// IntType multiply member function with other UDTs as parameters - END ======
-
-// IntType divide member function with other UDTs as parameters - BEGIN ======
-
-IntType& IntType::divide(const FloatType& ft)
-{
-    return divide(*ft.ownedFloat);
-}
-
-IntType& IntType::divide(const DoubleType& dt)
-{
-    return divide(*dt.ownedDouble);
-}
-
-IntType& IntType::divide(const IntType& it)
-{
-    return divide(*it.ownedInt);
-}
-
-// IntType divide member function with other UDTs as parameters - END ========
-*/
-
 int main()
 {
+    FloatType ft1(1.6f);
+    DoubleType dt1(0.81234);
+    IntType it1(23);
+
+    std::cout << std::endl;
+    std::cout << "FloatType ft1\t : " << ft1 << std::endl;
+    std::cout << "DoubleType dt1\t : " << dt1 << std::endl;
+    std::cout << "IntType it1\t\t : " << it1 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "ft1/dt1\t\t\t : " << static_cast<double>(ft1) / dt1  << std::endl;
+    std::cout << "(ft1+dt1)*10\t : " << static_cast<float>( ft1.add( static_cast<float>(dt1) ).multiply( static_cast<float>(10) ) ) << std::endl;
+    std::cout << "(dt1-it1)*it1\t : " << static_cast<double>( dt1.subtract(it1).divide( static_cast<double>(it1) ) ) << std::endl;
+
+    FloatType ft2 = 0.f;
+    DoubleType dt2 = 0.000987;
+    IntType it2 = 0;
+    std::cout << std::endl;
+    std::cout << "FloatType ft2\t : " << ft2 << std::endl;
+    std::cout << "DoubleType dt2\t : " << dt2 << std::endl;
+    std::cout << "IntType it2\t\t : " << it2 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "ft2/dt2\t\t\t : " << static_cast<double>(ft2) / dt2 << std::endl;
+    std::cout << "dt2/ft2\t\t\t : " << dt2 / static_cast<double>(ft2) << std::endl;
+    //std::cout << "it2*dt2-12\t\t : " << it2.multiply(dt2).subtract(12)<< std::endl;
+    std::cout << "it2*dt2-12\t\t : " << dt2.multiply( static_cast<double>(it2) ).subtract(12)<< std::endl;
+
+    FloatType ft3 = 3.1f;
+    DoubleType dt3 = 3.14159;
+    IntType it3 = 2;
+    std::cout << std::endl;
+    std::cout << "FloatType ft3\t : " << ft3 << std::endl;
+    std::cout << "DoubleType dt3\t : " << dt3 << std::endl;
+    std::cout << "IntType it3\t\t : " << it3 << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "ft3*it3-dt3\t\t : " << ft3.multiply( static_cast<float>(it3) ).subtract( static_cast<float>(dt3) )<< std::endl;
     
-    FloatType ft(1.6f);
-    DoubleType dt(0.81234);
-    IntType it(23);
     std::cout << std::endl;
-    std::cout << "FloatType ft\t : " << *ft.ownedFloat << std::endl;
-    std::cout << "DoubleType dt\t : " << *dt.ownedDouble << std::endl;
-    std::cout << "IntType it\t\t : " << *it.ownedInt << std::endl;
+    std::cout << "FloatType ft3\t : " << ft3 << std::endl;
+    std::cout << "DoubleType dt3\t : " << dt3 << std::endl;
+    std::cout << "IntType it3\t\t : " << it3 << std::endl;
 
     std::cout << std::endl;
-    std::cout << "ft/dt\t\t\t : " << *ft.ownedFloat / *dt.ownedDouble << std::endl;
-    std::cout << "(ft+dt)*10\t\t : " << *ft.add(dt).multiply(10).ownedFloat << std::endl;
-    std::cout << "(dt-it)*it\t\t : " << *dt.subtract(it).divide(it).ownedDouble << std::endl;
-
-    *ft.ownedFloat = 0.f;
-    *dt.ownedDouble = 0.000987;
-    *it.ownedInt = 0;
-    std::cout << std::endl;
-    std::cout << "FloatType ft\t : " << *ft.ownedFloat << std::endl;
-    std::cout << "DoubleType dt\t : " << *dtErnest.ownedDouble << std::endl;
-    std::cout << "IntType it\t\t : " << *it.ownedInt << std::endl;
+    std::cout << "ft3*(it3-dt3)\t : " << ft3 * it3.subtract( static_cast<int>(dt3) ) << std::endl;
 
     std::cout << std::endl;
-    std::cout << "ft/dt\t\t\t : " << *ft.ownedFloat / *dt.ownedDouble << std::endl;
-    std::cout << "dt/ft\t\t\t : " << *dt.ownedDouble / *ft.ownedFloat << std::endl;
-    std::cout << "it*dt-12\t\t : " << *it.multiply(dt).subtract(12).ownedInt << std::endl;
-
-    *ft.ownedFloat = 3.1f;
-    *dt.ownedDouble = 3.14159;
-    *it.ownedInt = 2;
-    std::cout << std::endl;
-    std::cout << "FloatType ft\t : " << *ft.ownedFloat << std::endl;
-    std::cout << "DoubleType dt\t : " << *dt.ownedDouble << std::endl;
-    std::cout << "IntType it\t\t : " << *it.ownedInt << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "ft*it-dt\t\t : " << *ft.multiply(it).subtract(dt).ownedFloat << std::endl;
-    
-    std::cout << std::endl;
-    std::cout << "FloatType ft\t : " << *ft.ownedFloat << std::endl;
-    std::cout << "DoubleType dt\t : " << *dt.ownedDouble << std::endl;
-    std::cout << "IntType it\t\t : " << *it.ownedInt << std::endl;
-
-    *ft.ownedFloat = 3.1f;
-    std::cout << std::endl;
-    std::cout << "FloatType ft\t : " << *ft.ownedFloat << std::endl;
-    std::cout << "DoubleType dt\t : " << *dt.ownedDouble << std::endl;
-    std::cout << "IntType it\t\t : " << *it.ownedInt << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "ft*(it-dt)\t\t : " << *ft.ownedFloat * (*it.subtract(dt).ownedInt) << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "FloatType ft\t : " << *ft.ownedFloat << std::endl;
-    std::cout << "DoubleType dt\t : " << *dt.ownedDouble << std::endl;
-    std::cout << "IntType it\t\t : " << *it.ownedInt << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "ft/0\t\t\t : " << *ft.divide(0).ownedFloat << std::endl;
-    std::cout << "dt/0\t\t\t : " << *dt.divide(0).ownedDouble << std::endl;
-    std::cout << "it/0\t\t\t : " << *it.divide(0).ownedInt << std::endl;
+    std::cout << "ft3/0\t\t\t : " << ft3.divide(0) << std::endl;
+    std::cout << "dt3/0\t\t\t : " << dt3.divide(0) << std::endl;
+    std::cout << "it3/0\t\t\t : " << it3.divide(0) << std::endl;
 
     std::cout << std::endl;
     std::cout << "good to go!" << std::endl;
